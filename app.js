dc024df2567b32c9ef38f273b70876adab2fb998
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-07-15 09:53:26 -0400

    Creo archivo app.js e inicio desarrollo

---
+++ app.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer')
+
+const tag = 'f1'
+const likes = 5
+let counter = 0
+
+const inicio = () => {
+  try {
+
+  } catch (error) {
+    console.log('error: ', error)
+  }
+}
+inicio()
