ac40e09724f6bdbef9d6df25ea680998c8e1a82a
rodolfovenegas <rodolfovenegas.es@gmail.com>
2021-07-15 11:17:45 -0400

    Agrego metodos para quitar alertas al iniciar sesion

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let counter = 0
 /*https://pptr.dev/*/
 const inicio = async () => {
   try {
-    // Abrir chrome
+    //? Abrir chrome
     const browser = await puppeteer.launch({
       headless: false,
       devtools: true
@@ -15,16 +15,23 @@ const inicio = async () => {
     // Nueva pagina
     const page = await browser.newPage()
     await page.goto('https://www.instagram.com/')
-    // Inicio sesion
+    //? Inicio sesion
+    // Espero a que el input este disponible 'waitForSelector'
     await page.waitForSelector('input[name="username"]', {visible: true})
+    // Estando visible empiezo a interactuar con los inputs
     await page.type('input[name="username"]', 'rodolfovcl', {delay:300})
     await page.type('input[type="password"]', '', {delay:300})
     await page.click('button[type="submit"]')
+    // Quitando alerta guardar inicio de sesion (esperando 1ro que este disponible el selector)
+    await page.waitForSelector('div[class="cmbtv"] > button', {visible: true})
+    await page.click('div[class="cmbtv"] > button')
+    // Quitando alerta activar notificaciones
+    await page.waitForSelector('div[class="mt3GC"] > button:nth-child(2)', {visible: true})
+    await page.click('div[class="mt3GC"] > button:nth-child(2)')
 
 
     // setTimeout(async() => { await page.screenshot({ path: 'captura.png' }) }, 1000)
     // setTimeout(async() => { await browser.close() }, 5000)
-
   } catch (error) {
     console.log('error: ', error)
   }
